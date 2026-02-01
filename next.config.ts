c50b6aaf0ec22e515b316188055d3afca82ce3cf
Faraz Hussain <farazhussain16@gmail.com>
2026-02-01 01:27:27 -0500

    Config file added.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // required for next/image on static export
+  images: {
+    unoptimized: true,
+  },
+
+  // helps routing on Firebase + direct refresh
+  trailingSlash: true,
 };
 
 export default nextConfig;
